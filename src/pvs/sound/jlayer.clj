3f74a72522a4c31bc91853113dbc2c648887e67e
Serguei Poliakov <serguei.poliakov@rogers.com>
2015-09-12 17:15:37 -0400

    new schedule

--- src/pvs/sound/jlayer.clj
+++ src/pvs/sound/jlayer.clj
@@ -1,9 +1,13 @@
 (ns pvs.sound.jlayer
-  (:import [javazoom.jl.player.advanced.AdvancedPlayer]))
+  (:import [javazoom.jl.player.advanced.AdvancedPlayer]
+))
 
 
 ;; tests
-(def zfile (str "file://" (.getCanonicalPath (java.io.File. ".")) "/resources/SHkolnyj_zvonok_-_SHkolnyj_zvonok_(get-tune.net).mp3"))
-(defn zstream [] (-> (java.net.URL. zfile) .openStream))
+(def xres (clojure.java.io/resource "SHkolnyj_zvonok_-_SHkolnyj_zvonok_(get-tune.net).mp3"))
+;;(def xfile 
+;;  (.getFile xres))
+;;(def zfile (str "file://" (.getCanonicalPath (java.io.File. ".")) "/resources/SHkolnyj_zvonok_-_SHkolnyj_zvonok_(get-tune.net).mp3"))
+(defn zstream [] (-> xres .openStream))
 (defn player [] (javazoom.jl.player.advanced.AdvancedPlayer. (zstream) 
                                  (.createAudioDevice (javazoom.jl.player.FactoryRegistry/systemRegistry))))
